2d865d751c14c1b2f1c47a05c3c5015a248a42b9
Emmanuel Neni <emmanuelneni@gmail.com>
2019-04-10 16:58:50 +0300

    set fixed state for shop page and made js for filter system

--- js/main.js
+++ js/main.js
@@ -4,3 +4,35 @@ $(document).ready(function () {
         $("nav ul").toggleClass("showing");
     });
 });
+
+
+
+// filter
+
+(function () {
+    const filterBtn = document.querySelectorAll('.filter-btn');
+
+    filterBtn.forEach(function (btn) {
+        btn.addEventListener("click", function (event) {
+            event.preventDefault();
+            const value = event.target.dataset.filter;
+
+            const items = document.querySelectorAll(".store-item")
+
+            items.forEach(function (item) {
+                if (value === "all") {
+                    items.style.display = "block";
+                } else {
+                    if (item.classList.contains(value)) {
+                        items.style.display = "block";
+                    } else {
+                        items.style.display = "none";
+                    }
+                }
+
+            });
+        });
+
+    });
+
+})();
