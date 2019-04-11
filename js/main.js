885d437006bad818a13bfa31853cb894fae52019
Emmanuel Neni <emmanuelneni@gmail.com>
2019-04-11 18:07:03 +0300

    some with all markup and basic design and javascript for shop page.

--- js/main.js
+++ js/main.js
@@ -40,3 +40,59 @@ $(document).ready(function () {
     });
 
 })();
+
+
+// show cart
+
+(function () {
+
+    const cartInfo = document.querySelector("cart-info");
+
+    const cart = document.querySelector("cart");
+
+    cartInfo.addEventListener("onmouseover", function () {
+        cart.classList.toggle("show-cart");
+    });
+
+})();
+
+// Working on the cart
+
+(function () {
+    const cartBtn = document.querySelectorAll("store-item-button");
+
+
+    cartBtn.forEach(function (btn) {
+        btn.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            if (event.target.parentElement.classList.contains("store-item-button")) {
+
+
+                let fullPath = event.target.parentElement.previousElementSibling.src;
+
+                let pos = fullPath.idexOf("img") + 3;
+
+
+                let partPath = fullPath.slice(pos);
+
+                const item = {};
+                item.img = "img-cart${partPath}";
+                let name = event.target.parentElement.parentElement.nextElement.children[0].textContent;
+
+                let finalPrice = price.slice(1).trim();
+
+
+                item.price = finalPrice;
+
+                const cartItem = document.createElement("div");
+
+
+
+
+            }
+
+        });
+
+    });
+})();
