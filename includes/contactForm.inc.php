9b7fdb6493de45741dca091c5ec957754ba04725
Emmanuel Neni <emmanuelneni@gmail.com>
2019-04-11 18:49:52 +0300

    created contact form and linked to all relevant pages

---
+++ includes/contactForm.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $carModel = $_POST['carModel'];
+    $mailFrom = $_POST['mail'];
+    $typeOfWork = $_POST['typeOfWork'];
+
+    $mailTo = 'deteylingvirus@gmail.com';
+    $headers = 'From: '.$mailFrom;
+    $txt = 'You received an email from '.$name.".\n\n".$typeOfWork;
+
+    mail($mailTo, $phone, $carModel, $txt, $headers);
+    header('Location: ../contact.php?MailSent');
+}
