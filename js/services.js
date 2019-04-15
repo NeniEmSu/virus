0b1a8c922ddaeb9be1eb4a76b7ac0eb576802f52
Emmanuel Neni <emmanuelneni@gmail.com>
2019-04-15 19:34:32 +0300

    used vue to eaily change the images for servces page

---
+++ js/services.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        image: '../img/services-car-goup-all.png',
+        description: ' white mercedes , jeep grand cherokee srt white & Black Range Rover Piano',
+        variants: [{
+                variantId: 01,
+                variantTitle: 'Ⅰ категорія',
+                variantDescription: 'середній бізнес спорт купе',
+                variantImage: '../img/services-car-goup-var-1.png'
+            },
+            {
+                variantId: 02,
+                variantTitle: 'ⅠⅠ категорія',
+                variantDescription: 'середній кросовер позашляховик',
+                variantImage: '../img/services-car-goup-var-2.png'
+            },
+            {
+                variantId: 03,
+                variantTitle: 'ⅠⅠⅠ категорія',
+                variantDescription: 'повнорозмірний кросовер позашляховик',
+                variantImage: '../img/services-car-goup-var-3.png'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
