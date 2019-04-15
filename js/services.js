f21c119b70ab5625f56074e62c17b5e933614c75
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-15 20:27:11 +0300

    changed the relative path for the images

--- js/services.js
+++ js/services.js
@@ -1,25 +1,25 @@
 var app = new Vue({
     el: '#app',
     data: {
-        image: '../img/services-car-goup-all.png',
+        image: './img/services-car-goup-all.png',
         description: ' white mercedes , jeep grand cherokee srt white & Black Range Rover Piano',
         variants: [{
                 variantId: 01,
                 variantTitle: 'Ⅰ категорія',
                 variantDescription: 'середній бізнес спорт купе',
-                variantImage: '../img/services-car-goup-var-1.png'
+                variantImage: './img/services-car-goup-var-1.png'
             },
             {
                 variantId: 02,
                 variantTitle: 'ⅠⅠ категорія',
                 variantDescription: 'середній кросовер позашляховик',
-                variantImage: '../img/services-car-goup-var-2.png'
+                variantImage: './img/services-car-goup-var-2.png'
             },
             {
                 variantId: 03,
                 variantTitle: 'ⅠⅠⅠ категорія',
                 variantDescription: 'повнорозмірний кросовер позашляховик',
-                variantImage: '../img/services-car-goup-var-3.png'
+                variantImage: './img/services-car-goup-var-3.png'
             }
         ],
         cart: 0
