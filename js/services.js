9a5767441f1dbf28fcd27bb7dbd6fa9026db6926
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-16 11:52:47 +0300

    added active classes to buttons for when clicked

--- js/services.js
+++ js/services.js
@@ -7,26 +7,30 @@ var app = new Vue({
                 variantId: 01,
                 variantTitle: 'Ⅰ категорія',
                 variantDescription: 'середній бізнес спорт купе',
-                variantImage: './img/services-car-goup-var-1.png'
+                variantImage: './img/services-car-goup-var-1.png',
+                isActive: false
             },
             {
                 variantId: 02,
                 variantTitle: 'ⅠⅠ категорія',
                 variantDescription: 'середній кросовер позашляховик',
-                variantImage: './img/services-car-goup-var-2.png'
+                variantImage: './img/services-car-goup-var-2.png',
+                isActive: false
             },
             {
                 variantId: 03,
                 variantTitle: 'ⅠⅠⅠ категорія',
                 variantDescription: 'повнорозмірний кросовер позашляховик',
-                variantImage: './img/services-car-goup-var-3.png'
+                variantImage: './img/services-car-goup-var-3.png',
+                isActive: false
             }
         ],
         cart: 0
     },
     methods: {
         changeCars(variantImage) {
-            this.image = variantImage
+            this.image = variantImage;
+            this.isActive = !this.isActive;
         }
     }
 })
