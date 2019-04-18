6c4240745e8a69decebfcd5c05d75d2597ef84b0
Emmanuel Neni <emmanuelneni@gmail.com>
2019-04-18 18:20:45 +0300

    all pf page uptimized to dynamicaly generate with vue

--- js/services.js
+++ js/services.js
@@ -51,7 +51,132 @@ let app = new Vue({
                 polishingClass: 'service-four'
             }
         ],
-        cart: 0
+        cleaners: [{
+                cleanersId: 05,
+                cleanersTitle: 'Хімчистка',
+                cleanersCost: 1999,
+                cleanersClass: 'service-five'
+
+            },
+            {
+                cleanersId: 06,
+                cleanersTitle: 'Хімчистка колесних арок, підвіски, дисків',
+                cleanersCost: 800,
+                cleanersClass: 'service-six'
+            },
+            {
+                cleanersId: 07,
+                cleanersTitle: 'Хімчистка передніх сидінь',
+                cleanersCost: 275,
+                cleanersClass: 'service-seven'
+            }, {
+                cleanersId: 08,
+                cleanersTitle: 'Хімчистка задніх сидінь',
+                cleanersCost: 495,
+                cleanersClass: 'service-eight'
+            }
+        ],
+        WaxCovers: [{
+                WaxCoversId: 09,
+                WaxCoversTitle: 'Покриття твердим універсальним воском Soft 99',
+                WaxCoversCost: 1200,
+                WaxCoversClass: 'service-nine'
+
+            },
+            {
+                WaxCoversId: 10,
+                WaxCoversTitle: 'Рідке скло',
+                WaxCoversCost: 3000,
+                WaxCoversClass: 'service-ten'
+            }
+
+        ],
+        CeramicCoating: [{
+            CeramicCoatingId: 11,
+            CeramicCoatingTitle: 'Керамічне покриття',
+            CeramicCoatingTitle: 'Покриття твердим універсальним воском Soft 99',
+            CeramicCoatingCost: 1200,
+            CeramicCoatingClass: 'service-eleven'
+        }],
+
+        additional: [{
+                additionalId: 13,
+                additionalTitle: 'Реставнація подряпин на кузові',
+                additionalCost: 130,
+                additionalClass: 'service-13'
+
+            },
+            {
+                additionalId: 14,
+                additionalTitle: 'Перешиття шкіряних деталей салону',
+                additionalCost: 500,
+                additionalClass: 'service-14'
+            },
+            {
+                additionalId: 15,
+                additionalTitle: 'Антидощ',
+                additionalCost: 600,
+                additionalClass: 'service-15'
+            },
+            {
+                additionalId: 16,
+                additionalTitle: 'Мийка мотору парою',
+                additionalCost: 500,
+                additionalClass: 'service-16'
+            },
+            {
+                additionalId: 17,
+                additionalTitle: 'Шумоізоляція',
+                additionalCost: 330,
+                dollar: true,
+                additionalClass: 'service-17'
+            },
+            {
+                additionalId: 18,
+                additionalTitle: 'Видалення запахів Бактерецидна обробка',
+                additionalCost: 500,
+                additionalClass: 'service-18'
+            },
+            {
+                additionalId: 19,
+                additionalTitle: 'Видалення вмятин без покраски PDR',
+                additionalCost: 0,
+                additionalClass: 'service-19'
+            },
+            {
+                additionalId: 20,
+                additionalTitle: 'Озонація',
+                additionalCost: 1000,
+                additionalClass: 'service-20'
+            },
+            {
+                additionalId: 21,
+                additionalTitle: 'Очистка кондиціонера',
+                additionalCost: 1200,
+                additionalClass: 'service-21'
+            },
+            {
+                additionalId: 22,
+                additionalTitle: 'Чистка дисків',
+                additionalCost: 250,
+                perItem: true,
+                additionalClass: 'service-22'
+            },
+            {
+                additionalId: 23,
+                additionalTitle: 'Керамічне покриття сидінь',
+                additionalCost: 500,
+                perItem: true,
+                additionalClass: 'service-23'
+            },
+            {
+                additionalId: 24,
+                additionalTitle: 'Озонація',
+                additionalCost: 1000,
+                additionalClass: 'service-24'
+            }
+        ]
+
     },
     methods: {
         changeCars(variantImage) {
